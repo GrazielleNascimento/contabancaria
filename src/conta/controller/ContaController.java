8127e18d11e0a0f8a64e73921a6a31ac9e6cdf49
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-24 19:34:20 -0300

    add ContaController

--- src/conta/controller/ContaController.java
+++ src/conta/controller/ContaController.java
@@ -7,69 +7,73 @@ import conta.repository.ContaRepository;
 
 public class ContaController implements ContaRepository {
 // Criamos uma Collection ArrayList, do tipo Conta (Classe Abstrata), chamada listaContas.
-	private ArrayList<Conta> listaContas = new ArrayList<Conta>();//Collection que irá armazenar os dados de todas as Contas, funcionando como um Banco de dados em Memória da nossa aplicação.
-	int numero = 0;//Criamos uma variável do tipo int chamada numero, armazenará o numero da última conta que foi criada.
+	private ArrayList<Conta> listaContas = new ArrayList<Conta>();// Collection que irá armazenar os dados de todas as
+																	// Contas, funcionando como um Banco de dados em
+																	// Memória da nossa aplicação.
+	int numero = 0;// Criamos uma variável do tipo int chamada numero, armazenará o numero da
+					// última conta que foi criada.
+
 	@Override
 	public void procurarPorNumero(int numero) {
 		var conta = buscarNaCollection(numero);
-		
-		if(conta != null)
+
+		if (conta != null)
 			conta.visualizar();
 		else
 			System.out.println("\n A conta número: " + numero + "não foi encontrada!");
 	}
-	
+
 	@Override
 	public void listarTodas() {
-		for(var conta : listaContas) {
+		for (var conta : listaContas) {
 			conta.visualizar();
 		}
-		
+
 	}
 
 	@Override
 	public void cadastrar(Conta conta) {
 		listaContas.add(conta);
 		System.out.println("\nA Conta número: " + conta.getNumero() + " foi criada com sucesso!");
-		
+
 	}
 
 	@Override
 	public void atualizar(Conta conta) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deletar(int numero) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void sacar(int numero, float valor) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void depositar(int numero, float valor) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public int gerarNumero() {
-		return ++ numero;
+		return ++numero;
 	}
-	
+
 	public Conta buscarNaCollection(int numero) {
-		for (var conta : listaContas ) {
+		for (var conta : listaContas) {
 			if (conta.getNumero() == numero) {
 				return conta;
 			}

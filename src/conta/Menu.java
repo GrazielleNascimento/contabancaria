dd76e528b69fa1187ed24d4349223a5c05495fd7
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-28 12:18:00 -0300

    implementa menu com o metodo depositar

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -186,9 +186,20 @@ public class Menu {
 				
 				keyPress();
 				break;
+				
 			case 7:
 				System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
-
+				System.out.println("Digite o numero da conta: ");
+				numero = leia.nextInt();
+				
+				do {//Foi criado um Laço de repetição do...while, para garantir que a variável valor receba via teclado um numero positivo e maior do que zero.
+					System.out.println("Digite o valor do depósito (R$): ");
+					valor = leia.nextFloat();
+				}while(valor <=0);
+				
+				//Foi criada a chamada para o Método depositar(), criado na Classe ContaController. Na chamada do Método foram inseridos 2 parâmetros:
+				contas.depositar(numero, valor);
+				
 				keyPress();
 				break;
 			case 8:
